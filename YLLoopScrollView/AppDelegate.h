0a3f5bac99b000a604ae57ce05fb75436101a2cd
yl@bdtrip.com.cn <yl@bdtrip.com.cn>
2016-03-18 09:38:01 +0800

    Initial Commit

---
+++ YLLoopScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YLLoopScrollView
+//
+//  Created by Youngly on 16/3/18.
+//  Copyright © 2016年 YL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YLLoopScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YLLoopScrollView
+//
+//  Created by Youngly on 16/3/18.
+//  Copyright © 2016年 YL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
